c8af27b4d6c769debe3a40fffbbe3159175ea615
Matheus Pinter <matheuspinter08@gmail.com>
2025-06-20 13:01:49 -0300

    feat: Added and almost finished about-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home-page/home.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { FuncionalidadePageComponent } from './funcionalidade-page/funcionalidade-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
   { path: 'home', component: HomeComponent },
   { path: 'funcionalidade', component: FuncionalidadePageComponent },
-  { path: 'contato', component: ContactPageComponent }
+  { path: 'contato', component: ContactPageComponent },
+  { path: 'sobre', component: AboutPageComponent }
 ];
